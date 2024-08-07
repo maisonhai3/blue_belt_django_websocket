dce8df4fbdb6b7079225d49090f37c1263bea23a
Mai Hai <maisonhai3@gmail.com>
2024-08-07 17:56:25 +0700

    Adapt Async consumer.

--- message_consumer/consumer.py
+++ message_consumer/consumer.py
@@ -3,31 +3,18 @@ from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 
 
 class MessageConsumer(AsyncWebsocketConsumer):
-    def connect(self):
-        self.accept()
-        self.send(text_data=json.dumps({
-            'message': 'Hello World!'
-        }))
-
     async def connect(self):
+        await self.accept()
         await self.send(text_data=json.dumps({
             'message': 'Hello World!'
         }))
-        await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        pass
 
     # Receive message from WebSocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message}
-        )
+        ...
 
     # Receive message from room group
     async def chat_message(self, event):
