0abca2b4a539a4071a9692f39ed02c4e6e18c32f
Mai Hai <maisonhai3@gmail.com>
2024-08-07 17:49:24 +0700

    Adapt Async consumer.

--- message_consumer/consumer.py
+++ message_consumer/consumer.py
@@ -1,16 +1,37 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 
 
-class MessageConsumer(WebsocketConsumer):
+class MessageConsumer(AsyncWebsocketConsumer):
     def connect(self):
         self.accept()
         self.send(text_data=json.dumps({
             'message': 'Hello World!'
         }))
 
-    def disconnect(self, close_code):
-        pass
+    async def connect(self):
+        await self.send(text_data=json.dumps({
+            'message': 'Hello World!'
+        }))
+        await self.accept()
 
-    def receive(self, text_data):
-        pass
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat.message", "message": message}
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message}))
